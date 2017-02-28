10e90908313fb92592d3ed689a335679dfdeebb0
Rui Santos <ee11077@fe.up.pt>
2017-02-28 12:14:02 +0000

    Comment fixed for IFTTT Api Key

--- code/23_Intruder_Detector_with_Notifications.ino
+++ code/23_Intruder_Detector_with_Notifications.ino
@@ -11,9 +11,12 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,1, 111);
 
+// REPLACE THE NEXT VARIABLE WITH YOUR IFTTT MAKER CHANNEL API KEY
+String iftttApiKey = "YOUR_MAKER_CHANNEL_API_KEY";
+
 // name address for IFTTT
 char server[] = "maker.ifttt.com";
-String iftttApiKey = "bAo0A6TL8jl3iAnk6sDg1S";
+
 // Initialize the Ethernet client library
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
